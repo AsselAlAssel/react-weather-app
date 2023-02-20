d92c15e149a8f5a1eef7286584a8ae881fd24080
Assel Al-Assel <aseel1234500000@gmail.com>
2023-02-20 11:00:14 +0200

    write the api and export it for reactQuery

---
+++ src/api.js
@@ -0,0 +1,12 @@
+const optionsGetCities = {
+    headers: {
+        'X-RapidAPI-Key': 'd7474c714dmsh473981e14d9ac7fp18a85ejsnab2bdf222cf8',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+const GEO_GET_CITIES_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const getCities = (inputValue) => {
+    return axios.get(`${GEO_GET_CITIES_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+        optionsGetCities
+    )
+}
