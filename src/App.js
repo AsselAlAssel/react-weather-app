15518e69d8f5608e3f452de602b782e7a563231c
Assel Al-Assel <aseel1234500000@gmail.com>
2023-02-20 10:59:47 +0200

    add search component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Search from './components/Search';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <Search />
     </div>
   );
 }
